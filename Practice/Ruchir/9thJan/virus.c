4487ceb57cd48867fe359fa8a90419552075276c
Ruchir Mehta <ruchir@smtp.gmail.com:465>
2022-01-09 17:56:46 +0530

    first version of virus\'s replicating behaviour

--- Practice/Ruchir/9thJan/virus.c
+++ Practice/Ruchir/9thJan/virus.c
@@ -1,41 +1,64 @@
 #include <stdlib.h>
 #include <stdio.h>
 #include <string.h>
+
 const char *fileSearch()
 {
     char *fileName = "victim.txt";
     return fileName;
 }
 
-void replicateVirus()
+void replicateVirus(char *content)
 {
-}
-int main()
-{
-    char *content = "to be written";
     long contentSize = strlen(content) + 1;
 
+    // Opening up victim in read and write mode
     FILE *fp = fopen(fileSearch(), "r+");
+
+    // Determining the size of victim
     fseek(fp, 0, SEEK_END);
     long fileSize = ftell(fp);
     fseek(fp, 0, SEEK_SET);
 
+    long combinedSize = contentSize + fileSize;
+
+    // For the content of the victim
     char *victimContent = malloc(fileSize + 1);
-    char copyContent[contentSize + fileSize + 1];
+    if (victimContent == NULL)
+    {
+        fprintf(stderr, "Memory not allocated\n");
+        exit(0);
+    }
 
+    // It contains the combined content to be copied
+    char copyContent[combinedSize + 1];
+
+    // Creating the content to be copied
     strcpy(copyContent, content);
-    fread(victimContent, fileSize, 1, fp);
+    int readElements = fread(victimContent, fileSize, 1, fp);
+    if (readElements != 1)
+    {
+        fprintf(stderr, "Error has occured or EOF reached\n");
+        exit(0);
+    }
+
     strcat(copyContent, victimContent);
+    copyContent[combinedSize] = 0;
 
-    copyContent[contentSize + fileSize] = 0;
-
+    /* Setting the file pointer to the beginning to paste the copyContent to the beginning of the victim */
     fseek(fp, 0, SEEK_SET);
     fprintf(fp, "%s", copyContent);
 
+    /*Releasing the space occupied in heap*/
     fclose(fp);
     free(victimContent);
+}
+int main()
+{
+    char *content = "to be written";
+    replicateVirus(content);
 
-    // TODO return value of malloc and fread
+    // TODO return value of fread
 
     return 0;
 }
