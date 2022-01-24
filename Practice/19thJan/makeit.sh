0b3cb3a552ad4f083027d0b3473f32cc38bc8dd1
iTeraTorr <cse180001044@iiti.ac.in>
2022-01-24 19:02:14 +0530

    24thJan

--- Practice/19thJan/makeit.sh
+++ Practice/19thJan/makeit.sh
@@ -4,5 +4,5 @@ readelf -a sweet > readelf.txt
 objdump -d sweet > disassembly_AT_and_T.txt
 objdump -M intel -d sweet > disassembly_intel.txt
 strace -v -o stracedFile.txt ./sweet > output.txt
-hexdump -C sweet > hexdump.txt
+hexdump -v -C sweet > hexdump.txt
 #objdump --full-contents --section=.comment sweet
